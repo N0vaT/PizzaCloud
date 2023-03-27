5f6014be4d92189b47b76ccc82dcb7b08a76a37f
N0vaT <Novikovto92@yandex.ru>
2023-03-27 13:25:21 +0300

    feat struct

---
+++ src/main/java/com/nova/pizzaCloud/PizzaCloudApplication.java
@@ -0,0 +1,13 @@
+package com.nova.pizzaCloud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PizzaCloudApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PizzaCloudApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/nova/pizzaCloud/PizzaCloudApplicationTests.java
@@ -0,0 +1,13 @@
+package com.nova.pizzaCloud;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PizzaCloudApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
