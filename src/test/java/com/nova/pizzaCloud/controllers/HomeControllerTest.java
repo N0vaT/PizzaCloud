c873161dbdd3f2ecc1b08c7de349d15cfbe45e4f
N0vaT <Novikovto92@yandex.ru>
2023-03-28 16:51:01 +0300

    feat WebConfig

--- src/test/java/com/nova/pizzaCloud/controllers/HomeControllerTest.java
+++ src/test/java/com/nova/pizzaCloud/controllers/HomeControllerTest.java
@@ -10,7 +10,7 @@ import static
         org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
 
-@WebMvcTest(HomeController.class)
+@WebMvcTest()
 public class HomeControllerTest {
 
     @Autowired
